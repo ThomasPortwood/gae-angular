bbd405984e0e1a3e00d15f1d43071d2c45de4055
Thomas Portwood <thomas.w.portwood@gmail.com>
2020-05-09 11:54:22 -0700

    angular start

---
+++ terraform/google-app-engine-application.tf
@@ -0,0 +1,4 @@
+resource "google_app_engine_application" "angular-sandbox" {
+  project = "sandbox-274623"
+  location_id = "us-west2"
+}
---
+++ terraform/terraform-providers.tf
@@ -0,0 +1,4 @@
+provider "google" {
+  credentials = file("/Users/thomasportwood/Downloads/Sandbox-85e9709a6284.json")
+  region = "us-west2"
+}
